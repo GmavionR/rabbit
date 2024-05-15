aeaa21990dbf3538331c8f4f3f437664419c58d7
weiwei <1254166074@qq.com>
2024-05-15 11:31:30 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import {watch} from 'vue'
+import {ref} from 'vue'
+
+const acount =ref(0)
+const acount1 =ref(0)
+
+const changeAcount =()=>{
+  acount.value++
+  acount1.value++
+}
+
+watch([acount,acount1],([acountn,acount1n],[acounto,acount1o])=>{
+  console.log(`新值${acountn},旧值${acounto}`)
+  console.log(`新值${acount1n},旧值${acount1o}`)
+})
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <div @click="changeAcount">{{ acount }}{{ acount1 }}</div>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
