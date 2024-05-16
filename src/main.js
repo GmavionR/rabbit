7f4ea6b0854f350ef4856cc7b63bef156a52b11b
weiwei <1254166074@qq.com>
2024-05-16 17:20:56 +0800

    图片懒加载-全局指令注册

--- src/main.js
+++ src/main.js
@@ -18,26 +18,8 @@ app.use(router)
 
 app.mount('#app')
 
-
-import { useIntersectionObserver } from '@vueuse/core'
-
-    // 懒加载指令逻辑
-    app.directive('img-lazy', {
-      mounted (el, binding) {
-        // el: 指令绑定的那个元素 img
-        // binding: binding.value  指令等于号后面绑定的表达式的值  图片url
-        console.log(el, binding.value)
-        const { stop } = useIntersectionObserver(
-          el,
-          ([{ isIntersecting }]) => {
-            console.log(isIntersecting)
-            if (isIntersecting) {
-              // 进入视口区域
-              el.src = binding.value
-            }
-          },
-        )
-      }
-    })
+//注册全局指令
+import { lazyPlugin } from '@/directives/index.js'
+app.use(lazyPlugin)
   
 
