6252562abd92e992464a2147239782e67f065c06
weiwei <1254166074@qq.com>
2024-05-17 14:18:46 +0800

    面包屑导航渲染

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,6 +3,7 @@ import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 import LayoutFixed from './components/LayoutFixed.vue'
+import Category from '../Category/index.vue'
 
 // 触发导航列表的action
 import { useCategoryStore } from '@/stores/category'
@@ -16,6 +17,7 @@ onMounted(() => {
 <template>
   <LayoutNav/>
   <LayoutHeader/>
+  <Category></Category>
   <LayoutFixed/>
   <!-- 展示二级路由 -->
   <RouterView/>
