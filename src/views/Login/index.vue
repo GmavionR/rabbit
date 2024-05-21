e7b3ea24ce919d73b44c9a091a72e8dfa80904e3
weiwei <1254166074@qq.com>
2024-05-21 17:02:21 +0800

    登录基础功能实现

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
+import {loginAPI} from '@/apis/user.js'
 // 1. 准备表单对象
 const form = ref({
   account: '18610848230',
@@ -32,22 +35,21 @@ const rules = {
   ]
 }
 // 3. 获取form实例做统一校验
-const formRef = ref(null)
 const router = useRouter()
+const formRef = ref(null)
 const doLogin = () => {
   const { account, password } = form.value
   // 调用实例方法
   formRef.value.validate(async (valid) => {
     // valid: 所有表单都通过校验  才为true
-    console.log(valid)
     // 以valid做为判断条件 如果通过校验才执行登录逻辑
     if (valid) {
       // TODO LOGIN
-    //   await userStore.getUserInfo({ account, password })
+      await loginAPI({ account, password })
       // 1. 提示用户
-    //   ElMessage({ type: 'success', message: '登录成功' })
+      ElMessage({ type: 'success', message: '登录成功' })
       // 2. 跳转首页
-    //   router.replace({ path: '/' })
+      router.replace({ path: '/' })
     }
   })
 }
@@ -75,7 +77,7 @@ const doLogin = () => {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"  :form="form" :rules="rules"
+            <el-form ref="formRef" label-position="right" label-width="60px"  :model="form" :rules="rules"
               status-icon>
               <el-form-item  label="账户" prop="account">
                 <el-input v-model="form.account"/>
@@ -88,7 +90,7 @@ const doLogin = () => {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
